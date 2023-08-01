cfcb9aa6e8aea77d6729261dca9aae1450d82df3
tesfa-ozem <alphatesfa789@gmail.com>
2023-08-01 18:57:53 +0300

    Add analytics endpoints

--- api/home/home.py
+++ api/home/home.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from fastapi import APIRouter, Response, Depends
 
-from core.fastapi.dependencies import PermissionDependency, AllowAll, IsAuthenticated
+from app.services.analytics import AnalyticsService
+from core.fastapi.dependencies import PermissionDependency, IsAuthenticated
 
 home_router = APIRouter()
 
@@ -11,3 +12,24 @@ home_router = APIRouter()
 )
 async def home():
     return Response(status_code=200)
+
+
+@home_router.get("/task-completion-rate")
+async def tasks_completion_rate():
+    analytics = AnalyticsService()
+    result = await analytics.calculate_task_completion_rate()
+    return {"task_completion_rate": result}, 200
+
+
+@home_router.get("/average_task_completion_time")
+async def average_task_completion_time():
+    analytics = AnalyticsService()
+    result = await analytics.calculate_average_task_completion_time()
+    return {"average_completion_time": result}, 200
+
+
+@home_router.get("/workforce_performance")
+async def workforce_performance():
+    analytics = AnalyticsService()
+    result = await analytics.calculate_average_task_completion_time()
+    return result, 200
