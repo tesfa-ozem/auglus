5c8da76a31015f24943575609431a71157b3e810
tesfa-ozem <alphatesfa789@gmail.com>
2023-08-01 17:46:59 +0300

    minor change in docker file

--- docker/api/startup.sh
+++ docker/api/startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 dockerize -wait tcp://db:5432 -timeout 20s
-pipenv alembic revision --autogenerate && pipenv run alembic upgrade head &&  pipenv run gunicorn --bind 0.0.0.0:8000 -w 4 -k uvicorn.workers.UvicornWorker app.server:app
+pipenv run alembic upgrade head &&  pipenv run gunicorn --bind 0.0.0.0:8000 -w 4 -k uvicorn.workers.UvicornWorker app.server:app
